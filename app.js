55114883fb5b6254b64be2666421e47c5ce33e40
christiansanchez-SE <christiansanchez714@gmail.com>
2024-10-01 21:33:08 -0400

    added js and py questions and answers

--- app.js
+++ app.js
@@ -171,3 +171,66 @@ var longestPalindrome = function(s) {
 };
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+// Zigzag Conversion
+
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+// And then read line by line: "PAHNAPLSIIGYIR"
+
+// Write the code that will take a string and make this conversion given a number of rows:
+
+// string convert(string s, int numRows);
+
+// Example 1:
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+
+// Example 2:
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+
+// Example 3:
+// Input: s = "A", numRows = 1
+// Output: "A"
+ 
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consists of English letters (lower-case and upper-case), ',' and '.'.
+// 1 <= numRows <= 1000
+
+var convert = function(s, numRows) {
+    // Edge case: if numRows is 1, return the original string
+    if (numRows === 1 || numRows >= s.length) {
+        return s;
+    }
+    
+    // Create an array to store the rows
+    let rows = new Array(numRows).fill('');
+    let currentRow = 0;
+    let goingDown = false;
+    
+    // Traverse the input string and populate the rows
+    for (let char of s) {
+        rows[currentRow] += char;
+        // Change direction when we reach the top or bottom row
+        if (currentRow === 0 || currentRow === numRows - 1) {
+            goingDown = !goingDown;
+        }
+        currentRow += goingDown ? 1 : -1;
+    }
+    
+    // Join all the rows to form the final string
+    return rows.join('');
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
--- app.py
+++ app.py
@@ -173,3 +173,68 @@ class Solution(object):
         return longest
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+# Zigzag Conversion
+
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+# And then read line by line: "PAHNAPLSIIGYIR"
+
+# Write the code that will take a string and make this conversion given a number of rows:
+
+# string convert(string s, int numRows);
+
+# Example 1:
+# Input: s = "PAYPALISHIRING", numRows = 3
+# Output: "PAHNAPLSIIGYIR"
+
+# Example 2:
+# Input: s = "PAYPALISHIRING", numRows = 4
+# Output: "PINALSIGYAHRPI"
+# Explanation:
+# P     I    N
+# A   L S  I G
+# Y A   H R
+# P     I
+
+# Example 3:
+# Input: s = "A", numRows = 1
+# Output: "A"
+ 
+# Constraints:
+
+# 1 <= s.length <= 1000
+# s consists of English letters (lower-case and upper-case), ',' and '.'.
+# 1 <= numRows <= 1000
+
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        # Edge case: if numRows is 1, return the original string
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        # Create an array of empty strings for each row
+        rows = [''] * numRows
+        current_row = 0
+        going_down = False
+        
+        # Traverse the input string and populate the rows
+        for char in s:
+            rows[current_row] += char
+            # Change direction when we reach the top or bottom row
+            if current_row == 0 or current_row == numRows - 1:
+                going_down = not going_down
+            current_row += 1 if going_down else -1
+        
+        # Join all the rows to form the final string
+        return ''.join(rows)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
